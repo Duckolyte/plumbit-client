9568bfb5b2d0acc51003624c67676b33a7c3219b
andygg <andreas.gebert1988@gmail.com>
2021-11-14 15:01:01 +0100

    Used span for material and work description row components. Added them to module.ts as componentEntries.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MaterialRowComponent } from './util/list/row/material-row/material-row.component';
-import { WorkDescriptionRowComponent } from './util/list/row/work-description-row/work-description-row.component';
-import { CountComponent } from './util/input/material/count/count.component';
-import { UnitComponent } from './util/input/material/unit/unit.component';
-import { NameComponent } from './util/input/material/name/name.component';
+import {AppComponent} from './app.component';
+import {MaterialRowComponent} from './util/list/row/material-row/material-row.component';
+import {WorkDescriptionRowComponent} from './util/list/row/work-description-row/work-description-row.component';
+import {CountComponent} from './util/input/material/count/count.component';
+import {UnitComponent} from './util/input/material/unit/unit.component';
+import {NameComponent} from './util/input/material/name/name.component';
 import {FormsModule} from '@angular/forms';
-import { GenericListComponent } from './util/list/material-list/generic-list.component';
-import { OrderViewComponent } from './view/order-view/order-view.component';
-import { GenericAddRowComponent } from './util/list/row/generic-add-row/generic-add-row.component';
-import { GenericEditRowComponent } from './util/list/row/generic-edit-row/generic-edit-row.component';
+import {GenericListComponent} from './util/list/material-list/generic-list.component';
+import {OrderViewComponent} from './view/order-view/order-view.component';
+import {GenericAddRowComponent} from './util/list/row/generic-add-row/generic-add-row.component';
+import {GenericEditRowComponent} from './util/list/row/generic-edit-row/generic-edit-row.component';
 import {RowTemplateDirective} from './util/list/row/row-template-directive';
-import { DescriptionComponent } from './util/input/work-description/description/description.component';
+import {DescriptionComponent} from './util/input/work-description/description/description.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +30,10 @@ import { DescriptionComponent } from './util/input/work-description/description/
     RowTemplateDirective,
     DescriptionComponent
   ],
+  entryComponents: [
+    MaterialRowComponent,
+    WorkDescriptionRowComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule
@@ -37,4 +41,5 @@ import { DescriptionComponent } from './util/input/work-description/description/
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
